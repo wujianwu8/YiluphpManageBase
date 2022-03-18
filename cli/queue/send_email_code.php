fb43a3f11f5b746c372f6ea6b815c3813f251d86
WuJianwu <7594870087@qq.com>
2022-03-19 00:37:39 +0800

    修改消息队列的BUG，完善model基类

--- cli/queue/send_email_code.php
+++ cli/queue/send_email_code.php
@@ -1,14 +1,15 @@
 <?php
 /*
  * 消息队列:发邮箱验证码
- * CLI模式下运行的示例文件，需要挂在后台一直运行着
- * 执行命令：php [目录路径]queue queue_name=你加入队列时取的队列名称
+ * 需要挂在后台一直运行着处理消息队列的守护进程
+ * 启动守护进程的方式：php /你的项目目录/queue "queue_name=你加入队列时取的队列名称"
+ * 如果要让队列在系统后台默默运行，在命令的最后面加一个与号就行了，这样执行：php /你的项目目录/queue "queue_name=你加入队列时取的队列名称" &
  * Created by PhpStorm.
  * User: WuJianwu
  * * Date: 2021/01/23
  * Time: 20:45
  */
-class send_email_code{
+class send_email_code {
 
     /**
      * @name 开始执行队列的函数
@@ -25,8 +26,12 @@ class send_email_code{
     public function run($msg)
     {
         if(empty($msg['to_alias']) || empty($msg['to_email']) || empty($msg['subject']) || empty($msg['html_body'])){
+            $msg = '发邮件验证码失败，参数错误，$msg:'.json_encode($msg);
+            if(PHP_SAPI=='cli'){
+                echo $msg."\r\n";
+            }
             //写文件日志
-            write_applog('ERROR', '发邮件验证码失败，参数错误，$msg:'.json_encode($msg));
+            write_applog('ERROR', $msg);
             return true;
         }
 
@@ -38,7 +43,11 @@ class send_email_code{
             tool_mailer::I()->auto_send();
         }
         catch (Exception $exception){
-            write_applog('ERROR', $exception->getMessage().'，$msg:'.json_encode($msg).', code:'.$exception->getCode());
+            $msg = $exception->getMessage().'，$msg:'.json_encode($msg).', code:'.$exception->getCode();
+            if(PHP_SAPI=='cli'){
+                echo $msg."\r\n";
+            }
+            write_applog('ERROR', msg);
         }
         return true;
     }
